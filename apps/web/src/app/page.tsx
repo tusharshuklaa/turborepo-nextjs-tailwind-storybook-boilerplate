01cc1a7c0bb75c23c17fd07c62663ef37fe9cece
Gaurav Vishnoi <gaurav.vishnoi@publicissapient.com>
2023-05-13 15:50:38 +0200

    Fix for client side component rendering

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,10 +1,16 @@
+'use client';
+
+// @use client
+// eslint-disable-next-line import/order
 import { Button } from "leafui";
+// Page.tsx
 import React from "react";
 
 const Page = () => (
-  <Button onClick={ () => { console.log('HI') } }>
+  <Button onClick={() => { console.log("HI"); }}>
     Hello
   </Button>
 );
 
 export default Page;
+
--- packages/leafui/src/components/button/Button.tsx
+++ packages/leafui/src/components/button/Button.tsx
@@ -1,25 +1,22 @@
-'use client';
-
-/* eslint-disable react/jsx-props-no-spreading */
-
-import React, { FC } from 'react';
+// Button.tsx
+import React, { FC, MouseEvent } from 'react';
 
 interface IButtonProps {
   children: React.ReactNode;
   className?: string;
-  onClick?: (e: MouseEvent) => void;
+  onClick?: (e: React.MouseEvent<HTMLButtonElement>) => void;
 }
 
 export const Button: FC<IButtonProps> = (props) => {
-  const { children, className='' } = props;
+  const { children, className = '', onClick } = props;
 
-    return (
-      <button
-        type="button"
-        className={ `bg-blue-500 hover:bg-blue-700 h-16 w-16 ${ className }` }
-        { ...props }
-      >
-        { children }
-      </button>
-    );
+  return (
+    <button
+      type="button"
+      className={`bg-blue-500 hover:bg-blue-700 h-16 w-16 ${className}`}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
 };
